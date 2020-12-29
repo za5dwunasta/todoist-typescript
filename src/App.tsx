6878d2932eec173e22ec278ae4ef6ec28fa08fb9
za5dwunasta <magdalena.motyl@gmail.com>
2020-12-29 12:33:53 +0000

    UX design implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
+import './App.scss'
+
+function App () {
   return (
-    <div className="App">
-      Start
+    <div className='App'>
+      <div className='todolist'>
+        <div className='header'>
+          <h1 className='header__name'>Todoist by Typecript</h1>
+          <p className='header__tasks-left'>You have 5 tasks left</p>
+        </div>
+        <div className='tasks'>
+          <div className='tasks__item'>
+            <input type='checkbox' id='task-1' />
+            <label htmlFor='task-1'>
+              <span className='custom-checkbox'></span>
+              first task
+            </label>
+          </div>
+          <div className='tasks__item'>
+            <input type='checkbox' id='task-2' />
+            <label htmlFor='task-2'>
+              <span className='custom-checkbox'></span>
+              next task
+            </label>
+          </div>
+          <div className='tasks__item'>
+            <input type='checkbox' id='task-3' />
+            <label htmlFor='task-3'>
+              <span className='custom-checkbox'></span>
+              another task
+            </label>
+          </div>
+        </div>
+        <div className='add-task'>
+          <button className='btn create' aria-label='create new task'>
+            +
+          </button>
+          <form action=''>
+            <input
+              type='text'
+              className='new task'
+              placeholder='new task name'
+              aria-label='new task name'
+            />
+          </form>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
